e9488e65824716e04e7ad4c51430b20639601410
Trần Đức Nam <me@dynamotn.dev>
2024-10-14 01:03:32 +0700

    refactor: copyright and fix Vietnamese words

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -33,7 +33,7 @@ export default {
       title: "Explorer",
     },
     footer: {
-      createdWith: "Created with",
+      createdWith: "Created by Dynamo with",
     },
     graph: {
       title: "Graph View",
