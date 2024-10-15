aa28f2f22b29e56729716012eb06c4670e2742d1
Trần Đức Nam <me@dynamotn.dev>
2024-10-16 00:34:58 +0700

    refactor: enable recent notes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,6 +37,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.RecentNotes(),
   ],
 }
 
